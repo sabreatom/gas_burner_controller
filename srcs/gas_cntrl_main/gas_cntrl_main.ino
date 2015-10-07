f92373588871815dd764e75e24d3e504682c9971
Aleksandrs <aleksandrs.maklakovs@gmail.com>
2015-10-07 23:51:51 +0300

    Added air quality and flame check functions.

--- srcs/gas_cntrl_main/gas_cntrl_main.ino
+++ srcs/gas_cntrl_main/gas_cntrl_main.ino
@@ -1,9 +1,88 @@
+//------------------------------------------------
+//Includes:
+--------------------------------------------------
+
+
+
+//------------------------------------------------
+//Defines:
+--------------------------------------------------
+
+#define AIR_SENSOR_0    0
+#deifne AIR_SENSOR_1    1
+#define AIR_Q_THRES     150
+
+#define FLAME_CH0       2
+#define FLAME_CH0       3
+#define FLAME_CH0       4
+#define FLAME_THRES     50
+
+//------------------------------------------------
+//Global variables:
+//------------------------------------------------
+
+
+
+//-----------------------------------------------
+//Setup function:
+//-----------------------------------------------
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
+
+//-----------------------------------------------
+//Main loop function:
+//-----------------------------------------------
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
+
+//------------------------------------------------
+//Check air quality:
+//------------------------------------------------
+
+boolean chck_air(adc_ch,air_thres)
+{
+  int data;
+
+  data = analogRead(adc_ch);
+
+  if (data <= air_thres)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+//------------------------------------------------
+//Check flame detection sensor:
+//------------------------------------------------
+
+boolean chck_flame(f_ch,f_thres)
+{
+  int data;
+
+  data = analogRead(f_ch);
+
+  if (data <= f_thres)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+//------------------------------------------------
+//Control ignition spark:
+//------------------------------------------------
+
+
+
+//------------------------------------------------

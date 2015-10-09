8e64ee6faef4429336d6ac0dd5404dcf2c1ad9d2
Aleksandrs <aleksandrs.maklakovs@gmail.com>
2015-10-09 19:46:26 +0300

    Added RC-5 command parsing, FSM states and modified sensor functions.

--- srcs/gas_cntrl_main/gas_cntrl_main.ino
+++ srcs/gas_cntrl_main/gas_cntrl_main.ino
@@ -2,33 +2,51 @@
 //Includes:
 --------------------------------------------------
 
-
+#include "RC5.h"
 
 //------------------------------------------------
 //Defines:
 --------------------------------------------------
 
-#define AIR_SENSOR_0    0
-#deifne AIR_SENSOR_1    1
+//#define AIR_SENSOR_0    0
+//#deifne AIR_SENSOR_1    1
 #define AIR_Q_THRES     150
 
-#define FLAME_CH0       2
-#define FLAME_CH0       3
-#define FLAME_CH0       4
+//#define FLAME_CH0       2
+//#define FLAME_CH1       3
+//#define FLAME_CH2       4
 #define FLAME_THRES     50
+
+#define IGN_CNTRL       2
+#define IGN_TIME        3 //ignition spark on time in seconds
+
+#define IR_PIN          3
+#define RC5_ADDR        16
+#define RC_ON_OFF       12
+
+enum fsm_states {fsm_idle,fsm_active,fsm_fault};
+fsm_states curr_state = fsm_idle;
 
 //------------------------------------------------
 //Global variables:
 //------------------------------------------------
 
-
+unsigned char rc5_prev_toogle = 1;
 
 //-----------------------------------------------
 //Setup function:
 //-----------------------------------------------
 
 void setup() {
+  //Serial port setup for debug during development:
+  Serial.begin(9600);
+  
+  //Setup ignition spark control pin:
+  pinMode(IGN_CNTRL,OUTPUT);
+  digitalWrite(IGN_CNTRL,LOW);
 
+  //Setup RC-5 receiver:
+  RC5 rc5(IR_PIN);
 }
 
 //-----------------------------------------------
@@ -36,53 +54,109 @@ void setup() {
 //-----------------------------------------------
 
 void loop() {
+  unsigned char rc5_toogle;
+  unsigned char rc5_address;
+  unsigned char rc5_command;
+  
+  switch(curr_state)
+  {
+    case fsm_idle:
+      if (rc5.read(&rc5_toogle,&rc5_address,&rc5_command))
+      {
+        if (rc5_address == RC5_ADDR)
+        {
+          if (rc5_toogle != rc5_prev_toogle)
+          {
+            if (rc5_command == RC_ON_OFF)
+            {
+              //Gass valve must be open max and with delay, so that there is enough gas
+              ignition_spark_cntrl(IGN_TIME);
+              if (chck_flame(FLAME_THRES))
+              {
+                //Gas valve must be set to typical position
+                curr_state = fsm_active;
+              }
+              else
+              {
+                //Gas valve must be turned off
+                curr_state = fsm_fault;
+              }
+            }
+          }
 
+          rc5_prev_toogle = rc5_toogle;
+        }
+      }
+
+      if (chck_air(AIR_Q_THRES))
+      {
+        curr_state = fsm_fault;
+        //Need to enable alarm audio signal
+      }
+      
+      break;
+    case fsm_active:
+      break;
+    case fsm_fault:
+      break;
+  }
 }
 
 //------------------------------------------------
 //Check air quality:
 //------------------------------------------------
 
-boolean chck_air(adc_ch,air_thres)
+boolean chck_air(unsigned int air_thres)
 {
-  int data;
+  unsigned int data;
+  unsigned int i;
 
-  data = analogRead(adc_ch);
+  for (i=0;i<2;i++)
+  {
+    data = analogRead(i);
 
-  if (data <= air_thres)
-  {
-    return false;
+    if (data > air_thres)
+    {
+      return true;
+    }
   }
-  else
-  {
-    return true;
-  }
+
+  return false;
 }
 
 //------------------------------------------------
 //Check flame detection sensor:
 //------------------------------------------------
 
-boolean chck_flame(f_ch,f_thres)
+boolean chck_flame(unsigned int f_thres)
 {
-  int data;
+  unsigned int data;
+  unsigned int i;
 
-  data = analogRead(f_ch);
+  for (i=2;i<5;i++)
+  {
+    data = analogRead(i);
 
-  if (data <= f_thres)
-  {
-    return false;
+    if (data <= f_thres)
+    {
+      return false
+    }
+
+    delay(5);
   }
-  else
-  {
-    return true;
-  }
+
+  return true
 }
 
 //------------------------------------------------
 //Control ignition spark:
 //------------------------------------------------
 
-
+void ignition_spark_cntrl(unsigned int spark_on_dly) //delay in seconds
+{
+  digitalWrite(IGN_CNTRL,HIGH); //Start ignition
+  delay(spark_on_dly*1000);
+  digitalWrite(IGN_CNTRL,LOW); //Start ignition
+}
 
 //------------------------------------------------
